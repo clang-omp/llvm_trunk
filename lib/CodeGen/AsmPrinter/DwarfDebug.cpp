5a71342955d543a3c9ee709aa0ceb1626b3fa338
Hal Finkel <hfinkel@anl.gov>
2015-03-27 18:46:51 -0500

    Revert "r215412 - Revert "Partially revert r214761 that asserted that all concrete debug info variables had DIEs, due to a failure on Darwin.""
    When using -g with -fopenmp, it is possible to hit this assert.

--- lib/CodeGen/AsmPrinter/DwarfDebug.cpp
+++ lib/CodeGen/AsmPrinter/DwarfDebug.cpp
@@ -492,7 +492,9 @@ void DwarfDebug::beginModule() {
 void DwarfDebug::finishVariableDefinitions() {
   for (const auto &Var : ConcreteVariables) {
     DIE *VariableDie = Var->getDIE();
-    assert(VariableDie);
+    // FIXME: There shouldn't be any variables without DIEs.
+    if (!VariableDie)
+      continue;
     // FIXME: Consider the time-space tradeoff of just storing the unit pointer
     // in the ConcreteVariables list, rather than looking it up again here.
     // DIE::getUnit isn't simple - it walks parent pointers, etc.
